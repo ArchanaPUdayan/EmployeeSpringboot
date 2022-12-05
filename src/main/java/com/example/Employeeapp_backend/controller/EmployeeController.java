8bcede45d46607cbcb404d521fcf4ee6fb6f9dae
Archanapudayan <atchanaudayan64534@gmail.com>
2022-12-05 15:55:25 +0530

    add

---
+++ src/main/java/com/example/Employeeapp_backend/controller/EmployeeController.java
@@ -0,0 +1,10 @@
+package com.example.Employeeapp_backend.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EmployeeController {
+    @GetMapping("/")
+
+}
